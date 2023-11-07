b3c65e702b75f469238b43794e2d6f7845d8c5e0
Carlos Mota <carlosmota81@gmail.com>
2023-11-06 22:57:45 -0300

    troca de dados com web socket

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+
+app.set("view engine","ejs");
+
+app.get("/", (req,res) =>{
+    res.render("index")
+})
+
+io.on("connection", (socket)=>{
+
+    socket.on("disconnect", ()=>{
+        console.log("desconectou" + socket.id);
+    })
+
+    socket.on("msg", (data) =>{
+        io.emit("showmsg", data)
+        console.log(data);
+    })
+
+})
+
+
+
+
+
+
+http.listen(3000, () =>{
+    console.log("App rodando");
+})
